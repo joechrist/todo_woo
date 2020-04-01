34342b225cc76e0828208390e00f25adcf8571dd
Joe <christowel@live.fr>
2020-03-31 21:19:55 -0400

    add some new logout fonctionality

--- todo/views.py
+++ todo/views.py
@@ -1,7 +1,47 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django.db import IntegrityError
+from django.contrib.auth import login, logout
+
+
+def home(request):
+    return render(request, 'todo/home.html')
 
 
 def signupuser(request):
-    return render(request, 'todo/signupuser.html',
-                  {'form': UserCreationForm()})
+    if request.method == 'GET':
+        return render(request, 'todo/signupuser.html',
+                      {'form': UserCreationForm()})
+    else:
+        # check if passwords match together
+        # Create the user and save him if not in the databases
+        # Check if the user name exist in de databases
+        if request.POST['password1'] == request.POST['password2']:
+            try:
+                user = User.objects.create_user(
+                    request.POST['username'],
+                    password=request.POST['password1'])
+                user.save()
+                login(request, user)
+                return redirect('currenttodos')
+            except IntegrityError:  # Primary key error - only one key
+                # Return the same page but with an error for the user
+                # Because there is already a user with the same name on db
+                return render(request, 'todo/signupuser.html',
+                              {'form': UserCreationForm(),
+                               'error': 'Sorry, user name already exist!'})
+        else:
+            return render(request, 'todo/signupuser.html',
+                          {'form': UserCreationForm(), 'error':
+                           'Sorry, password didn\'t match!'})
+
+
+def logoutuser(request):
+    if request.method == 'POST':
+        logout(request)
+        return redirect('home')
+
+
+def currenttodos(request):
+    return render(request, 'todo/currenttodos.html')
