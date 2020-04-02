52e934c683848df8aa6929ceb37e7d7e5a4459cd
Joe <christowel@live.fr>
2020-04-02 17:31:38 -0400

    add delete and complete todo features

--- todowoo/urls.py
+++ todowoo/urls.py
@@ -29,4 +29,8 @@ urlpatterns = [
     # Todos
     path('current/', views.currenttodos, name='currenttodos'),
     path('create/', views.createtodo, name='createtodo'),
+    path('todo/<int:todo_pk>', views.viewtodo, name='viewtodo'),
+    path('todo/<int:todo_pk>/complete',
+         views.completetodo, name='completetodo'),
+    path('todo/<int:todo_pk>/delete', views.deletetodo, name='deletetodo'),
 ]
